e05662db2161444d97328003e6f282a36c1d2a52
Manuelb1990 <manuell@bluewin.ch>
2022-03-04 21:44:15 +0100

    add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'sales-tool-8bdf7',
+    appId: '1:669663510131:web:308932209770ba1234f406',
+    storageBucket: 'sales-tool-8bdf7.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDiAwTvwgu8Y5BKiwCrMUNz6ZOGUmCi8HM',
+    authDomain: 'sales-tool-8bdf7.firebaseapp.com',
+    messagingSenderId: '669663510131',
+    measurementId: 'G-G6N93DTPN7',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'sales-tool-8bdf7',
+    appId: '1:669663510131:web:308932209770ba1234f406',
+    storageBucket: 'sales-tool-8bdf7.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDiAwTvwgu8Y5BKiwCrMUNz6ZOGUmCi8HM',
+    authDomain: 'sales-tool-8bdf7.firebaseapp.com',
+    messagingSenderId: '669663510131',
+    measurementId: 'G-G6N93DTPN7',
+  },
   production: false
 };
 
